3cd9e8cf48c10e5bda7b04facfe90acc96877f41
MLDODSON <paola@dominicansoul.com>
2024-01-12 11:37:11 -0600

    Branch 5
    Branch 5 - The shoe detail layout.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav";
+import Shoe from "./components/Shoe";
 
 const App = () => {
   return (
     <div className="p-10 xl:px-24">
       <Nav/>
+      <Shoe/>
     </div>
   )
 }
