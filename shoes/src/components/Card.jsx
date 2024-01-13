371583c722b144303ac434c2a6b1a4417a6a1712
MLDODSON <paola@dominicansoul.com>
2024-01-13 16:06:19 -0600

    Branch 16
    Branch 16 - Dynamically selecting a shoe.

--- shoes/src/components/Card.jsx
+++ shoes/src/components/Card.jsx
@@ -1,6 +1,7 @@
-const Card = ({ item }) => {
+const Card = ({ item, onClick }) => {
     return (
-        <div className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}>
+        <div onClick={() => onClick(item)} 
+            className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}>
             <div className="p-8">
                 <div className="text-2xl font-bold">{item.title}</div>
                 <div className="mt-10 font-semibold underline underline-offset-4">SHOP NOW</div>
